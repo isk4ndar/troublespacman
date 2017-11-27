e643899e67948bcddc074dafe719e5bb856cbd7e
Bruno Silva <brunosilv_a@outlook.com>
2017-11-26 23:11:16 -0300

    last changes to presentation.

--- troublespacman/Assets/Scripts/Pontuacao.cs
+++ troublespacman/Assets/Scripts/Pontuacao.cs
@@ -10,15 +10,13 @@ public class Pontuacao : MonoBehaviour
     public static int current;
 
     void Start()
-    {
-        //inicializador das variaveis de contagem
+    {   //inicializador das variaveis de contagem
         pontos = 0;  //pontos totais/current high score
         current = 0; //pontos torais/high score
     }
 
     void Update()
     {
-        //atualiza o contado a cada frame e
         //incremento da variavel ponto
         pontosTXT.text = pontos.ToString();
     }
